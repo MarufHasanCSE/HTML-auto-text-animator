79c6dd262160558dc779e515f0aabb3b56a0d637
Md Maruf Hasan <marufhasancse21@gmail.com>
2026-02-02 23:33:30 +0600

    Almost Done!

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 const elem1 = document.querySelector('.container h1');
 const text1 = 'I am a Student';
 let index1 = 0;
+let displayedText = '';
+let isAnimating = false;
 
 function autoText1() {
     if (index1 < text1.length) {
-      
-        elem1.innerHTML += text1.charAt(index1);
+        displayedText += text1.charAt(index1);
+        elem1.textContent = displayedText;
         index1++;
-        setTimeout(autoText1, 200);
+        setTimeout(autoText1, 100);
+    } else {
+        isAnimating = false;
     }
 }
 
-autoText1();    
+elem1.addEventListener('click', function() {
+    if (!isAnimating) {
+        isAnimating = true;
+        index1 = 0;
+        displayedText = '';
+        elem1.textContent = '';
+        autoText1();
+    }
+});
+
+window.addEventListener('load', function() {
+    autoText1();
+});    
