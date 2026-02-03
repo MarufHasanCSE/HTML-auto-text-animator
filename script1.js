1ad7a031603038b6989c05ab1a350363247f003d
Md Maruf Hasan <marufhasancse21@gmail.com>
2026-02-03 10:30:01 +0600

    Completed!

--- script1.js
+++ script1.js
@@ -1,21 +1,24 @@
 const containerElem = document.querySelector(".container");
 
+const careers = ["Student", "Web Developer", "Animator", "Coder", "Teaching Assistant", "Fresh Graduate"];
 
-const careers = ["Student", "Web Developer", "Coder", "Teaching Assistant"];
 
 let careerIndex = 0;
 let charIndex = 0;
 
 function updateText() { 
       charIndex++;
-      containerElem.innerHTML = `<h1> I am a ${careers[careerIndex].slice(0,charIndex)}</h1>`;
+      containerElem.innerHTML = `<h1> I am ${careers[careerIndex].slice(0,1) === "A" ? "an" : "a"} ${careers[careerIndex].slice(0,charIndex)}</h1>`;
   
    if(charIndex === careers[careerIndex].length){
         careerIndex++;
         charIndex = 0;
 
    }
-    setTimeout(updateText,300);
+   if(careerIndex  === careers.length){
+        careerIndex = 0;
+   }
+    setTimeout(updateText, 200);
 }
 
 updateText();
